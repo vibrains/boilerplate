b839dd7df928856868054b7b1cb39986a812b9a5
LMI <lmi@LMIs-MacBook-Pro.local>
2017-02-20 22:21:33 -0500

    geonmaes edits

--- js/geonames_request.js
+++ js/geonames_request.js
@@ -1,6 +1,8 @@
 var url="http://api.geonames.org/postalCodeLookupJSON?postalcode=6600&country=AT&username=demo";
 $.getJSON(url, function (data) {
 	$.each(data.postalcodes, function (){
-		$('ul#results').append('<li><div class=\"cityName\">' +this.placeName+ '</div></li>', '<li>Longitude: <div class=\"lng\">' +this.lng+ '</div></li>', '<li class=\"latitude\">Latitude: <div class=\"lat\">' +this.lat+ '</div></li>');
+		$('ul#results').append('<li><div class=\"cityName\">' +this.placeName+ '</div></li>', 
+			'<li>Longitude: <div class=\"lng\">' +this.lng+ '</div></li>',
+			'<li class=\"latitude\">Latitude: <div class=\"lat\">' +this.lat+ '</div></li>');
 	});
 });
